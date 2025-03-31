e9a214b83b801724af2e27a000743fbf3879d76a
Mr.Kira-Tamil <132659616+MrKira007@users.noreply.github.com>
2025-03-31 22:56:28 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// In your Vite or app configuration
-app.listen(8080, '0.0.0.0', () => {
-  console.log("Server is running on port 8080");
-});
 
 // https://vitejs.dev/config/
 export default defineConfig({
