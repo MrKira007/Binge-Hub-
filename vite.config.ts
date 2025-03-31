8e9eeaaa2b9caf7684da9b6ce5fdd83ca5d0f03e
Mr.Kira-Tamil <132659616+MrKira007@users.noreply.github.com>
2025-03-31 23:36:27 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: "terser",
+    chunkSizeWarningLimit: 1000, // Prevents warnings for large files
+  },
 });
